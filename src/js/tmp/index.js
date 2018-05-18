1491d523acc04deb161eb189c45924690f9de7d5
yyl <yiyulong@live.com>
2018-05-18 20:59:59 +0800

    touchEvent

--- src/js/tmp/index.js
+++ src/js/tmp/index.js
@@ -2,7 +2,7 @@
  * @Author: yyl 
  * @Date: 2018-05-04 20:37:02 
  * @Last Modified by: yyl
- * @Last Modified time: 2018-05-17 20:42:35
+ * @Last Modified time: 2018-05-18 20:30:03
  */
 //  动画效果执行一次
 // $.fn.extend({
@@ -126,7 +126,7 @@ function bgAnimate () {
     bg_box.removeClass('box_enter').addClass('box_shake');
   }, 600);
   setTimeout(() => {
-    pageplay('.page1')
+    pageplay('.page7')
   }, 800);
 }
 // pageplay
@@ -134,7 +134,7 @@ function pageplay (el) {
   $(el).siblings().css('display', 'none').end().css('display', 'block');
 }
 $('body').height($(window).height())
-$('.page1 .go').on('click', function () {
+$('.page1 .go').on('touchstart', function () {
   let user = $('.page1 input').val()
   if (user) pageplay('.page2')
   $('.user').text(user)
@@ -147,6 +147,8 @@ $('.page3 .go').on('click', function () {
 })
 $('.page4 .go').on('click', function () {
   pageplay('.page5')
+  // marquee()
+  mask_square()
 })
 $('.page6 .go').on('click', function () {
   pageplay('.page7')
@@ -167,21 +169,25 @@ $('.page8 .share').on('click', function () {
   pageplay('.page9')
 })
 var actorNo = 0;
-$('.actor').on('click', function () {
+var actor_timer = null
+$('.page3 .actor').on('click', function () {
+  // if (actor_timer) return
+  // actor_timer = setTimeout(() => {
+  //   console.log('timer')
+  //   actor_timer = null
+  // }, 1000);
+  if ($('.page3 .actor.active').length >= 3) return
   let flag = $(this).hasClass('active');
-  // if (actorNo == 3) {
-    // pageplay('.page4')
-  // } else {
-    if (flag) {
-      $(this).removeClass('active');
-      actorNo--
-    } else {
-      $(this).addClass('active');
-      actorNo == 2 ? pageplay('.page4') : actorNo++
-      // actorNo++
-    }
-    console.log(actorNo)
-  // }
+  flag ? $(this).removeClass('active') : $(this).addClass('active');
+  let length = $('.page3 .actor.active').length
+  if (length == 3) {
+    $('.page3 .logo').addClass('active');
+    setTimeout(() => {
+      pageplay('.page4');
+      $('.page3 .logo').removeClass('active')
+      $('.page3 .actor').removeClass('active')
+    }, 1000);
+  };
 });
 $('.page7 .diy_element li').on('click', function () {
   let index = $(this).index()
@@ -195,3 +201,93 @@ $('.page7 .diy_right li').on('click', function () {
   $(this).addClass('active').siblings().removeClass('active')
   $('.diy_bg img').attr('src', src + index + '.png')
 })
+function marquee () {
+  let num = 0;
+  var timer = setInterval(function() {
+    num++
+    // console.log(num)
+    $('.page5 .bg').toggleClass('active')
+    if (num == 9) {
+      clearInterval(timer)
+      pageplay('.page6')
+    }
+  }, 160)
+}
+function mask_square () {
+  let num = 0
+  let cont = 0
+  var timer = setInterval(function() {
+    if (num == 29) {
+      num = 0
+      cont++
+    } else {
+      num++
+    }
+    if (cont == 2) {
+      // clearInterval(timer)
+      // speed = 1000
+      // timer
+      if (num == 3 || num == 6 || num == 20) {
+        $('.page5 .mask_square span').eq(num).addClass('active')
+      } else {
+        $('.page5 .mask_square span').eq(num).addClass('active').end().eq(num - 1).removeClass('active')
+      }
+    } else if(cont == 3) {
+      clearInterval(timer)
+      marquee()
+    } else {
+      $('.page5 .mask_square span').eq(num).addClass('active').end().eq(num - 1).removeClass('active')
+    }
+  }, 70)
+}
+var ZMF = (function (doc) {
+  return {
+    copyDom: null,
+    events: function () {
+      $('.page7 .labels img').on('touchstart', this.diyStart)
+      $('.page7 .labels img').on('touchmove', this.diyMove)
+      $('.page7 .labels img').on('touchend', this.diyEnd)
+    },
+    diyStart: function () {
+      let _this = ZMF;
+      let domRect = $(this)[0].getBoundingClientRect();
+      _this.copyDom = $(this).clone();
+      let css = {
+        'width': domRect.width + 'px',
+        'height': domRect.height + 'px',
+        'left': domRect.left + 'px',
+        'top': domRect.top + 'px'
+      }
+      _this.copyDom.css(css).appendTo('.page7');
+    },
+    diyMove: function (e) {
+      let _this = ZMF;
+      let _touch = e.originalEvent.targetTouches[0];
+      let offsetX = parseInt(_this.copyDom.css('width')) / 2;
+      let offsetY = parseInt(_this.copyDom.css('height')) / 2;
+      let _x = _touch.pageX - offsetX + 'px';
+      let _y = _touch.pageY - offsetY + 'px';
+      _this.copyDom.css({'left': _x, 'top': _y});
+    },
+    diyEnd: function () {
+      let _this = ZMF;
+      let diyContainer = $('.page7 .diy_left')[0];
+      let rect = diyContainer.getBoundingClientRect();
+      let domRect = _this.copyDom[0].getBoundingClientRect();
+      let domClass = _this.copyDom.attr('class');
+      let _offsetTop = rect.top <= domRect.top ? true : false;
+      let _offsetLeft = rect.left <= domRect.left ? true : false;
+      let _offsetRight = rect.right >= domRect.right ? true : false;
+      let _offsetBottom = rect.bottom >= domRect.bottom ? true : false;
+      let _left = (parseInt(_this.copyDom.css('left')) - rect.left) + 'px';
+      let _top = (parseInt(_this.copyDom.css('top')) - rect.top) + 'px';
+      if (_offsetTop && _offsetLeft && _offsetRight && _offsetBottom) {
+        $('.page7 .diy_content').children('.' + domClass).remove()
+        _this.copyDom.appendTo($('.page7 .diy_content')).css({'left': _left, 'top': _top});
+      } else {
+        _this.copyDom.remove();
+      }
+    }
+  }
+})(document)
+ZMF.events()
